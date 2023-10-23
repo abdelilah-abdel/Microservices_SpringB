08168dd6859b82ea5a1d8d1854b158799e1dbffc
Abdelilah Ait Hamou <aah@createitreal.com>
2023-10-23 18:47:04 +0100

    tp2

---
+++ api_SB/api_SB/src/main/java/com/myHR/api_SB/ApiSbApplication.java
@@ -0,0 +1,13 @@
+package com.myHR.api_SB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSbApplication.class, args);
+	}
+
+}
---
+++ api_SB/api_SB/src/test/java/com/myHR/api_SB/ApiSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myHR.api_SB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
